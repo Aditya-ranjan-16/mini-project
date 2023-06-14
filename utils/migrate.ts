f0107bb6e15953f79e91f7c7edc480d8ec147d87
Aditya-ranjan-16 <2005428@kiit.ac.in>
2023-06-15 01:10:43 +0530

    fix

--- utils/migrate.ts
+++ utils/migrate.ts
@@ -4,7 +4,7 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
-config({ path: ".env" });
+config({ path: ".env.local" });
 
 const databaseUrl = drizzle(
   postgres(`${process.env.DATABASE_URL}`, { ssl: "require", max: 1 })
